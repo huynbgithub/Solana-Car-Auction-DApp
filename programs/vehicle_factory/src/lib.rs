a00c7167da41ab97ea680ab89b36f417ce78a6bc
Huy Nguyễn Bá <nguyenbahuytk@gmail.com>
2023-11-06 12:59:00 +0700

    Update lib.rs

--- programs/vehicle_factory/src/lib.rs
+++ programs/vehicle_factory/src/lib.rs
@@ -1,24 +1,20 @@
 use anchor_lang::prelude::*;
-use vehicle::cpi::accounts::Initialize;
-use vehicle::program::Vehicle;
-use vehicle::VehicleProperties;
-use vehicle::{self, VehicleData};
 
-declare_id!("95w6ZbaNJV2sJzDuQQxE3wqshfyE3Y1Q3PHJWDdMCHWF");
+declare_id!("Eu3pnSQvXw3jhLeyBcDdkY6Y6m6pD8vP4to5SmdtZcXh");
 
 #[program]
 pub mod vehicle_factory {
     use super::*;
 
-    pub fn initialize(ctx: Context<InitializeFactory>) -> Result<()> {
-        let vehicle_factory = &mut ctx.accounts.vehicle_factory;
-        let owner = &mut ctx.accounts.signer;
+    // pub fn initialize(ctx: Context<VehicleFactory>) -> Result<()> {
+    //     let vehicle_factory = &mut ctx.accounts.vehicle_factory;
+    //     let owner = &mut ctx.accounts.signer;
 
-        vehicle_factory.deployed_vehicles = Vec::new();
-        vehicle_factory.owner = *owner.key;
+    //     vehicle_factory.deployed_vehicles = Vec::new();
+    //     vehicle_factory.owner = *owner.key;
 
-        Ok(())
-    }
+    //     Ok(())
+    // }
 
     pub fn create_vehicle(
         ctx: Context<CreateVehicle>,
@@ -26,44 +22,51 @@ pub mod vehicle_factory {
         starting_price: u128,
         vehicle_images: Vec<String>,
     ) -> Result<()> {
-        vehicle::cpi::initialize(
-            ctx.accounts.initialize_ctx(),
-            props,
-            starting_price,
-            vehicle_images,
-        )
+        let vehicle = &mut ctx.accounts.vehicle;
+        let owner = &mut ctx.accounts.signer;
+
+        vehicle.owner = *owner.key;
+        vehicle.is_start = false;
+        vehicle.is_approved = false;
+        vehicle.props = props;
+        vehicle.starting_price = starting_price;
+        vehicle.vehicle_images = vehicle_images;
+        vehicle.bids = Vec::new();
+        vehicle.bids_size = 0;
+
+        Ok(())
     }
 }
+
+// #[derive(Accounts)]
+// pub struct VehicleFactory<'info> {
+//     #[account(
+//         init,
+//         payer = signer,
+//         space = 2000
+//     )]
+//     pub vehicle_factory: Account<'info, VehicleFactoryData>,
+
+//     #[account(mut)]
+//     pub signer: Signer<'info>,
+
+//     pub system_program: Program<'info, System>,
+// }
+
+// #[account]
+// pub struct VehicleFactoryData {
+//     owner: Pubkey,
+//     deployed_vehicles: Vec<Pubkey>,
+// }
 
 #[derive(Accounts)]
 pub struct CreateVehicle<'info> {
-    #[account(mut)]
-    pub vehicle: Account<'info, VehicleData>,
-    pub vehicle_program: Program<'info, Vehicle>,
-    pub signer: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-impl<'info> CreateVehicle<'info> {
-    pub fn initialize_ctx(&self) -> CpiContext<'_, '_, '_, 'info, Initialize<'info>> {
-        let cpi_program = self.vehicle_program.to_account_info();
-        let cpi_accounts = Initialize {
-            vehicle: self.vehicle.to_account_info(),
-            signer: self.signer.to_account_info(),
-            system_program: self.system_program.to_account_info(),
-        };
-        CpiContext::new(cpi_program, cpi_accounts)
-    }
-}
-
-#[derive(Accounts)]
-pub struct InitializeFactory<'info> {
     #[account(
         init,
         payer = signer,
         space = 2000
     )]
-    pub vehicle_factory: Account<'info, VehicleFactory>,
+    pub vehicle: Account<'info, VehicleData>,
 
     #[account(mut)]
     pub signer: Signer<'info>,
@@ -72,7 +75,39 @@ pub struct InitializeFactory<'info> {
 }
 
 #[account]
-pub struct VehicleFactory {
+pub struct VehicleData {
     owner: Pubkey,
-    deployed_vehicles: Vec<String>,
+    is_start: bool,
+    is_approved: bool,
+    props: VehicleProperties,
+    starting_price: u128,
+    vehicle_images: Vec<String>,
+    bids: Vec<Bid>,
+    bids_size: u32,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct VehicleProperties {
+    owner_full_name: String,
+    owner_address: String,
+    brand: String,
+    vehicle_type: String,
+    color: String,
+    seat_capacity: u16,
+    origin: String,
+    license_plate: String,
+    engine_number: String,
+    chassis_number: String,
+    model_code: String,
+    capacity: u32,
+    first_registration_date: u128,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct Bid {
+    index: u32,
+    bidder: Pubkey,
+    quantity: u128,
+    bid_date: u128,
+    is_withdrawed: bool,
 }
